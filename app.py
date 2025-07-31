9753316c1f50714edfc0ddb997b27ccedde9b4e9
jelemiekun - pc <jeremieeden1946@gmail.com>
2025-07-31 20:29:15 +0800

    feat: math lib

--- app.py
+++ app.py
@@ -1,33 +1,5 @@
-first_number = float(input("First Number: "))
-second_number = float(input("Second Number: "))
+import math
 
-operation_dialogue = """
-Select operation:
-1 - Addition
-2 - Subtraction
-3 - Multiplication
-4 - Division
-5 - Modulus
-6 - Exponent
-"""
-print(operation_dialogue)
-operation_selected = int(input("Choose operation: "))
-
-result = 0
-result = float(result)
-
-match operation_selected:
-    case 1:
-        result = first_number + second_number
-    case 2:
-        result = first_number - second_number
-    case 3:
-        result = first_number * second_number
-    case 4:
-        result = first_number / second_number
-    case 5:
-        result = first_number % second_number
-    case 6:
-        result = first_number ** second_number
-
-print(f"Result: {result}")
+boolVal = True
+result = boolVal
+print(float(result))
