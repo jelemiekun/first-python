434e6a62ea6d822b6012a6ba65ef85d76137ccc3
jelemiekun - pc <jeremieeden1946@gmail.com>
2025-07-31 13:20:43 +0800

    feat: string methods

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
-first_name = "Meow"
-last_name = "Arf"
-message = f"{first_name} [{last_name}]"
+first_name = input("First Name: ")
+last_name = input("Last Name: ")
+age = input ("Age: ")
+age = int(age)
+find_name = input("Find name: ")
 
-if len(message) > 15:
-    print("Too many characters!")
+message = f"{first_name} {last_name} is {age} years old."
+
+print(message)
+print(f"The above message contains {len(message)} characters")
+print(message.lower().replace("john".lower(), "Johnnn"))
+
+found = find_name.lower() in message.lower()
+
+if found:
+    print(f"{find_name} found.")
 else:
-    print(message.upper())
+    print(f"Unable to find {find_name}.")
